aae1c1b20de00779be962a36de3d62ed4b3d80c0
hd132626 <hd132626@LG-PC>
2015-04-13 16:34:35 +0900

    다섯번째 과제 제출

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -5,7 +5,7 @@ public class Loop {
 		for(int i=1;i<=10;i++) {
 			for(int j=1;j<=10;j++) {
 				if(j==i) System.out.printf("*");
-				else System.out.printf("%d", j);
+				else System.out.print( j);
 			} 
 			System.out.println();
 		}
