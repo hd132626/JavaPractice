3165399bbfd9c6a1871232ab4cd860ba9c2b9206
hd132626 <hd132626@LG-PC>
2015-08-28 16:46:05 +0900

    두번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,18 @@
+package org.dimigo.inheritance;
+
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new IPhone("IPhone6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction(phone);
+			phone.turnOff();
+			System.out.println();
+		}
+	}
+}
