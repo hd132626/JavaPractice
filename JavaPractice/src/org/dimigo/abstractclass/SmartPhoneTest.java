2e6a71cd760d8c05a126dcb7bbb9917f169ee9ed
hd132626 <hd132626@LG-PC>
2015-09-03 11:15:53 +0900

    abstract class 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,20 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new IPhone("IPhone6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction(phone);
+			phone.turnOff();
+			System.out.println();
+			
+		}
+	}
+}
